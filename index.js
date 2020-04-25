684882878aeee7326459f5cb77cca2d540b09ee8
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-04-25 20:29:06 +0300

    Barebone of the project

---
+++ index.js
@@ -0,0 +1,7 @@
+//go to instagram
+//search by query
+//form to enter query
+//show user results
+//add button to queries they want to use
+//copy all selected hashtags
+//allow maximum of thirty hashtags
