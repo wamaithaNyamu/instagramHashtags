430cf71ec4bbfc2c3440749315bf6d0ee46b9d95
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-04-29 20:07:40 +0300

    heroku create igbotwams

--- routes/hashtags.routes.js
+++ routes/hashtags.routes.js
@@ -2,10 +2,10 @@ module.exports = (app) =>{
     const hashtags = require('../controllers/hashtags.controller.js');
 
     //get all hashtags
-    app.get('igbot.herokuapp.com/', hashtags.findAll);
+    app.get('igbotwams.herokuapp.com/', hashtags.findAll);
 
     //get on hashtag
-    app.get('igbot.herokuapp.com/:hashtag', hashtags.findOne);
+    app.get('igbotwams.herokuapp.com/:hashtag', hashtags.findOne);
 
 
 }
